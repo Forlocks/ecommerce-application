c3060455c333e056125eabaf6965e393fea88e68
Aleksey <volgaopa@gmail.com>
2024-05-03 19:38:16 +0300

    feat: add husky hook pre-commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 import './index.scss';
-
-const a = 'test huskystest huskytesоt huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest husky'
