6bf6acd4faeaa32cf812c40099694c5012ef7704
Aleksey <volgaopa@gmail.com>
2024-05-03 19:15:40 +0300

    feat: set up husky hooks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import './index.scss';
 
-'test huskystest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest husky'
+const a = 'test huskystest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest huskytest husky'
