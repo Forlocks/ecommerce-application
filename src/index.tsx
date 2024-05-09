f6880b12a5bd0dc150cb97a5726dcc21a22fb1f0
Aleksey <volgaopa@gmail.com>
2024-05-09 08:26:28 +0300

    test lint-staged working

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
-import "./index.scss";
-import ErrorPage from "./pages/error-page/error-page";
-import MainPage from "./pages/main-page/main-page";
-import RegisterPage from "./pages/register-page/register-page";
-import LoginPage from "./pages/login-page/login-page";
+} from 'react-router-dom';
+import './index.scss';
+import ErrorPage from './pages/error-page/error-page';
+import MainPage from './pages/main-page/main-page';
+import RegisterPage from './pages/register-page/register-page';
+import LoginPage from './pages/login-page/login-page';
 
 const root = document.createElement('div');
 root.setAttribute('id', 'root');
@@ -16,22 +16,24 @@ document.body.append(root);
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainPage />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
   },
   {
-    path: "/register",
-    element: <RegisterPage />
+    path: '/register',
+    element: <RegisterPage />,
   },
   {
-    path: "/login",
-    element: <LoginPage />
+    path: '/login',
+    element: <LoginPage />,
   },
 ]);
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
+'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import './error-page.scss';
 
 export default function ErrorPage() {
