997622b11bb8190305b16041ec5f04e043310e53
Aleksey <volgaopa@gmail.com>
2024-05-09 08:30:39 +0300

    feat: test lint-staged working for tsx ext

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.scss';
 import ErrorPage from './pages/error-page/error-page';
 import MainPage from './pages/main-page/main-page';
@@ -15,25 +12,23 @@ root.setAttribute('id', 'root');
 document.body.append(root);
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
+	{
+		path: '/',
+		element: <MainPage />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/register',
+		element: <RegisterPage />,
+	},
+	{
+		path: '/login',
+		element: <LoginPage />,
+	},
 ]);
 
 ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 );
-
-'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
