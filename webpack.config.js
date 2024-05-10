42cd58defe04ce2970ba18d3d0056a15704938d7
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-05-10 10:48:24 +0300

    feat: ECOMM-Alexey888888-1 Add SDK

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = (env) => {
   const isDev = env.dev === true;
@@ -35,6 +36,9 @@ module.exports = (env) => {
     },
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        'node-fetch': "isomorphic-fetch",
+      }
     },
     plugins: [
       new HtmlWebpackPlugin({
@@ -57,7 +61,8 @@ module.exports = (env) => {
           }
         ],
       }),
-      new CleanWebpackPlugin({cleanStaleWebpackAssets: false})
+      new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+      new Dotenv()
     ],
     devServer: isDev ? {
       open: true,
