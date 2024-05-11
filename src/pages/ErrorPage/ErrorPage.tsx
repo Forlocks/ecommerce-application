f845d03528451f01b4fe58120236aeb0f2134ab2
Aleksey <106630486+Forlocks@users.noreply.github.com>
2024-05-12 00:45:36 +0300

    feat: ECOMM-2_20 Implement not found page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 import './ErrorPage.scss';
 
 export const ErrorPage: React.FC = function () {
-  return <div></div>;
+  return (
+    <div className="error-page">
+      <h1 className="error-page__title">
+        404
+        <br />
+        Page not found
+      </h1>
+      <Link className="error-page__link" to="/">
+        Back to the main page?
+      </Link>
+      <img
+        className="error-page__image"
+        src="../../assets/images/brokenVase.png"
+        alt="Broken vase"
+      />
+    </div>
+  );
 };
