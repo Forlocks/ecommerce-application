69a7509eabbba5db41b861ff81ba8f61fd137c04
Alexey888888 <brdkmail@gmail.com>
2024-05-14 15:00:18 +0300

    fix: fix login functionality after input invalid password

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { MainPage } from './pages/MainPage/MainPage';
 import { RegistrationPage } from './pages/RegistrationPage/RegistrationPage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { Layout } from './components/visual/layout/Layout';
-import { User } from './controllers/api/User';
+import { User } from './controllers/Api/User';
 
 const root = document.createElement('div');
 root.setAttribute('id', 'root');
