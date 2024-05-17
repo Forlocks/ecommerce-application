df92fcb7c5b595fbc0628691ef6f8906f617e84b
Alexey888888 <119805836+Alexey888888@users.noreply.github.com>
2024-05-17 09:39:16 +0300

    feat: ECOMM-Alexey888888-5 Add user name and date of birth component

--- src/components/non-visual/validators/validators.ts
+++ src/components/non-visual/validators/validators.ts
@@ -31,3 +31,29 @@ export const validatePassword = (password: string): string => {
   }
   return '';
 };
+
+export const validateName = (name: string): string => {
+  const trimmedName = name.trim();
+  if (!trimmedName) return 'this field cannot be empty';
+  if (!/^[A-Za-z\s]*$/.test(trimmedName)) {
+    return 'this field must not contain special characters or numbers';
+  }
+  return '';
+};
+
+export const validateDateOfBirth = (date: string): string => {
+  if (!date) return 'date of birth cannot be empty';
+  const dateOfBirthString = date;
+  const dateOfBirthObj = new Date(dateOfBirthString);
+  const todayDate = new Date();
+  let age = todayDate.getFullYear() - dateOfBirthObj.getFullYear();
+  if (
+    todayDate.getMonth() < dateOfBirthObj.getMonth() ||
+    (todayDate.getMonth() === dateOfBirthObj.getMonth() &&
+      todayDate.getDate() < dateOfBirthObj.getDate())
+  ) {
+    age--;
+  }
+  if (age < 13) return 'You must be 13 years old or older';
+  return '';
+};
